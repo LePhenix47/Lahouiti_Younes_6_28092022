53d407ff901f29adb1c5f74e79d000b7c146e3fc
Younes Lahouiti <loller763@gmail.com>
2022-10-13 08:44:52 +0200

    Ajout de tabindexs aux posts

--- js/Templates/photographer-template.js
+++ js/Templates/photographer-template.js
@@ -42,14 +42,14 @@ class PhotographerProfileTemplate {
         <div class="images__post-container" data-post-id="${id}" data-photographers-id="${photographerId}"
             data-publishing-date="${date}" data-likes="${likes}" data-user-liked="false" data-title="${title}">
             <div class="images__post">
-              <a href="#" title="${title}" aria-label="Image nommée ${title}" role="link">
+              <a href="#" title="${title}" aria-label="Image nommée ${title}" role="link" tabindex="0">
                 <img class="images__image" src="../assets/images/Posts photos/${image}"
                   alt="'${title}' fait en ${new Date(date).getFullYear()}" />
               </a>
               <div class="images__post-text">
-                <p class="images__post-description">${title}</p>
+                <p class="images__post-description" tabindex="0">${title}</p>
                 <button class="images__post-like-button" title="Mettre un like au post '${title}'?" aria-pressed="false"
-                  aria-label="Bouton pour liker la publication nommée '${title}'">${likes} <i
+                  aria-label="Bouton pour liker la publication nommée '${title}'" tabindex="0">${likes} <i
                     class="fa-solid fa-heart"></i></button>
               </div>
             </div>
@@ -64,14 +64,14 @@ class PhotographerProfileTemplate {
 
             <div class="images__post">
 
-              <a href="#" title="${title}" aria-label="Video nommée ${title}" role="link">
+              <a href="#" title="${title}" aria-label="Video nommée ${title}" role="link" tabindex="0">
                 <video src="../assets/images/Posts photos/${video}" class="images__video">
                 </video>
               </a>
               <div class="images__post-text">
-                <p class="images__post-description">${title}</p>
+                <p class="images__post-description" tabindex="0">${title}</p>
                 <button class="images__post-like-button" title="Mettre un like au post '${title}'?" aria-pressed="false"
-                  aria-label="Bouton pour liker la publication nommée '${title}'">${likes} <i
+                  aria-label="Bouton pour liker la publication nommée '${title}'" tabindex="0">${likes} <i
                     class="fa-solid fa-heart"></i></button>
               </div>
 
